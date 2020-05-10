5105eda2fcd9ae1ab27cccbc3aa4b547dc9cb839
Crowdbotics <team@crowdbotics.com>
2020-05-10 08:23:05 +0000

    Update models

--- is_livraison_16831/wsgi.py
+++ is_livraison_16831/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'is_livraison_16831.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "is_livraison_16831.settings")
 
 application = get_wsgi_application()
